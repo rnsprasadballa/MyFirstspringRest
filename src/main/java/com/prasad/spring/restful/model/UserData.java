936411e2ae91ac41c78fbae807cc0b2b1a5318bf
Rama Naga Satya Prasad Balla <rnsprasadballa@users.noreply.github.com>
2015-04-10 09:19:33 +0530

    Create UserData.java

---
+++ src/main/java/com/prasad/spring/restful/model/UserData.java
@@ -0,0 +1,59 @@
+package com.prasad.spring.restful.model;
+
+import java.io.Serializable;
+
+public class UserData implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String companyName;
+	private String country;
+	private String userId;
+	private String password;
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
